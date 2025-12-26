950a95708b3b044f312dda22e531fbf41be79630
hw <rodionkasinov735@gmail.com>
2025-12-26 23:08:32 +0100

    my hw for 2 lessson

---
+++ HW lesson 2.py
@@ -0,0 +1,49 @@
+# # ex_1
+# number = int(input("Enter your number: "))
+# n1 = number ** 2
+# print("your number is ", n1)
+
+# # ex_2
+# number = int(input("The first number: "))
+# number_2 = int(input("The second number: "))
+# number_3 = int(input("The third number: "))
+# n1 = number
+# n2 = number_2
+# n3 = number_3
+# average = (n1 + n2 + n3)/3
+# print("The average is", average)
+
+# # ex_3
+# time = int(input("Enter the minutes: "))
+# n1 = time //60
+# n2 = time %60
+# print(f"{n1} hours and {n2} minutes")
+
+# # ex_4
+# prise = int(input("Enter the prise: "))
+# sale = int(input("Enter the sale: "))
+# prise_1 = prise - (sale/100 * prise  )
+# print("New prise is : ",prise_1)
+
+# # ex_5
+# number = int(input("Enter a number: "))
+# n1 = number // 10
+# n2 = number % 10
+# print("Your answer is " , n2)
+
+# # ex_6
+# side_a = int(input("Enter the side a: "))
+# side_b = int(input("Enter the side b: "))
+# per_of_r = (side_a + side_b) * 2
+# print("perimeter of a rectangle is", per_of_r)
+
+# # ex_7
+# number = int(input("Enter a four-digit number: "))
+# n1 = number//1000
+# n2 = (number//100)%10
+# n3 = (number//10)%10
+# n4 = number%10
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
