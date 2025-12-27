629152a24c3be2b90a925bae59623bb794b8ffad
hw <rodionkasinov735@gmail.com>
2025-12-27 15:06:24 +0100

    my ex

---
+++ DOP.py
@@ -0,0 +1,55 @@
+# # my_own_ex_1
+# number = int(input("Enter numbers of weeks: "))
+# days = number * 7
+# print("Answer is",days)
+# #
+# number = int(input("Enter a number: "))
+# if number > 10:
+#     print("Number is more than 10")
+# else :
+#     print("Number is less than 10")
+
+
+
+
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print("четное ")
+# else:
+#     print("не чет ")
+
+
+
+# Если x % 2 == 0 → число четное
+#
+# Если x % 2 != 0 → число нечетное
+
+
+
+
+# n1 = int(input("Enter a number: "))
+# n2 = int(input("Enter one more number: "))
+# n3 = int(input("And Enter one more number: "))
+#
+# if n1 > n2 and n1 > n3:
+#     print(n1)
+# elif n2 > n1 and n2 > n3:
+#     print(n2)
+# else :
+#     print(n3)
+
+
+
+# # determing the time of day
+# number = int(input("Enter the time 0 to 23:"))
+# if number == 0 or number < 6:
+#     print("night")
+#
+# elif number == 6 or number > 11:
+#     print("morning")
+#
+# elif number == 12 or number > 17:
+#     print("afternoon")
+#
+# else:
+#     print("evening")
